99bb80358aa2edd2d267a8ed16fface82becaa9c
Gideon Adeoye <gadeoye@feedgy.solar>
2024-12-16 16:59:34 +0100

    build: create contacts table

--- dbt_data_eng/models/jafflegaggle_contacts.sql
+++ dbt_data_eng/models/jafflegaggle_contacts.sql
@@ -19,7 +19,7 @@ order_events as (
     SELECT 
         user_id,
         min(timestamp) as first_order,
-        max(timestamp) as most_recent_event,
+        max(timestamp) as most_recent_order,
         count(event_id) as number_of_orders
     FROM events
     where event_name = 'order_placed'
@@ -35,8 +35,9 @@ final_all_users as (
         users.created_at,
         user_events.first_event,
         user_events.most_recent_event,
-        user_events.first_order,
-        order_events.most_recent_event,
+        user_events.number_of_events,
+        order_events.first_order,
+        order_events.most_recent_order,
         order_events.number_of_orders,
         if(users.email_domain in {{ personal_emails }}, null, users.email_domain)
             as corporate_email
@@ -54,5 +55,28 @@ final_merged_users as (
         coalesce(fmu.email, fau.email) as email,
         coalesce(fmu.corporate_email, fau.corporate_email) as corporate_email,
 
-        A
+        ARRAY_AGG(fau.user_name ORDER BY fau.created_at desc)[1] as user_name,
+
+
+        {#- 
+            Note that a user can be associated with multiple gaggles depending on the merge,
+            but the max() below explicitly will take only the later gaggle as associated.
+        #}
+        max(fau.gaggle_id) as gaggle_id,
+
+        min(fau.created_at) as created_at,
+        min(fau.first_event) as first_event,
+        min(fau.most_recent_event) as most_recent_event,
+        sum(fau.number_of_events) as number_of_events,
+        min(fau.first_order) as first_order,
+        min(fau.most_recent_order) as most_recent_order,
+        sum(fau.number_of_orders) as number_of_orders,
+        substring(cast(fau.created_at as varchar), 1, 10) as user_cohort
+        
+    from final_all_users fau
+    left join {{ ref('merged_user') }} mu on fau.email = mu.old_email
+    left join final_all_users fmu on fmu.email = mu.new_email
+    group by 1, 2, 3, 13
 )
+
+select * from final_merged_users
